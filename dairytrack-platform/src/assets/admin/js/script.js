73d12815c28db1c02c762ef7580100ae21637a78
Christian Yehezkil Gultom <tiantomm@gmail.com>
2025-03-21 21:19:24 +0700

    terbaru

--- dairytrack-platform/src/assets/admin/js/script.js
+++ dairytrack-platform/src/assets/admin/js/script.js
@@ -1,62 +1,23 @@
-$(function() {
+/*------------------------------------------------------------------
+* Bootstrap Simple Admin Template
+* Version: 3.0
+* Author: Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-------------------------------------------------------------------*/
+(function() {
+    'use strict';
 
-    // Sidebar Toggler
-    function sidebarToggle(toogle) {
-        var sidebar = $('#sidebar');
-        var padder = $('.content-padder');
-        if( toogle ) {
-            $('.notyf').removeAttr( 'style' );
-            sidebar.css({'display': 'block', 'x': -300});
-            sidebar.transition({opacity: 1, x: 0}, 250, 'in-out', function(){
-                sidebar.css('display', 'block');
-            });
-            if( $( window ).width() > 960 ) {
-                padder.transition({marginLeft: sidebar.css('width')}, 250, 'in-out');
-            }
-        } else {
-            $('.notyf').css({width: '90%', margin: '0 auto', display:'block', right: 0, left: 0});
-            sidebar.css({'display': 'block', 'x': '0px'});
-            sidebar.transition({x: -300, opacity: 0}, 250, 'in-out', function(){
-                sidebar.css('display', 'none');
-            });
-            padder.transition({marginLeft: 0}, 250, 'in-out');
-        }
-    }
-
-    $('#sidebar_toggle').click(function() {
-        var sidebar = $('#sidebar');
-        var padder = $('.content-padder');
-        if( sidebar.css('x') == '-300px' || sidebar.css('display') == 'none' ) {
-            sidebarToggle(true)
-        } else {
-            sidebarToggle(false)
-        }
+    // Toggle sidebar on Menu button click
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar').toggleClass('active');
+        $('#body').toggleClass('active');
     });
 
-    function resize()
-    {
-        var sidebar = $('#sidebar');
-        var padder = $('.content-padder');
-		padder.removeAttr( 'style' );
-		if( $( window ).width() < 960 && sidebar.css('display') == 'block' ) {
-			sidebarToggle(false);
-		} else if( $( window ).width() > 960 && sidebar.css('display') == 'none' ) {
-			sidebarToggle(true);
-		}
-    }
+    // Auto-hide sidebar on window resize if window size is small
+    // $(window).on('resize', function () {
+    //     if ($(window).width() <= 768) {
+    //         $('#sidebar, #body').addClass('active');
+    //     }
+    // });
+})();
 
-    if($( window ).width() < 960) {
-        sidebarToggle(false);
-    }
-
-	$( window ).resize(function() {
-		resize()
-	});
-
-    $('.content-padder').click(function() {
-        if( $( window ).width() < 960 ) {
-            sidebarToggle(false);
-        }
-    });
-
-})
--- dairytrack-platform/src/auth/login.js
+++ dairytrack-platform/src/auth/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import styles from "../assets/admin/css/login.module.css";
+import styles from "../assets/auth/css/login.module.css";
 
 const Login = () => {
   const navigate = useNavigate();
