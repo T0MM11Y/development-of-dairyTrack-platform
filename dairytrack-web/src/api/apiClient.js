cb3df4aa0f7653e8c946606a1aacf92125b1291b
Hasan Sinaga <sinagahasan17@gmail.com>
2025-03-31 17:54:10 +0700

    consume api jenis pakan,pakan, stok

--- dairytrack-web/src/api/apiClient.js
+++ dairytrack-web/src/api/apiClient.js
@@ -1,7 +1,11 @@
 const BASE_URL = "http://127.0.0.1:5002/api";
 
 //pny T0mm11y
+<<<<<<< HEAD
 // const BASE_URL = "http://127.0.0.1:5000/api";
+=======
+const BASE_URL = "http://127.0.0.1:8000/api";
+>>>>>>> c5bd6d9a (api hasan)
 
 export const fetchAPI = async (endpoint, method = "GET", data = null) => {
   const options = {
@@ -15,23 +19,28 @@ export const fetchAPI = async (endpoint, method = "GET", data = null) => {
     options.body = JSON.stringify(data);
   }
 
-  const response = await fetch(`${BASE_URL}/${endpoint}`, options);
-  const contentType = response.headers.get("content-type");
+  try {
+    const response = await fetch(`${BASE_URL}/${endpoint}`, options);
+    const contentType = response.headers.get("content-type");
 
-  if (!response.ok) {
-    if (contentType && contentType.includes("application/json")) {
-      const errorData = await response.json();
-      throw new Error(errorData.detail || "Something went wrong.");
-    } else {
-      const errorText = await response.text(); // tangani HTML error
-      console.error("Server Error:", errorText);
-      throw new Error("Internal Server Error");
+    if (!response.ok) {
+      if (contentType && contentType.includes("application/json")) {
+        const errorData = await response.json();
+        throw new Error(errorData.detail || "Something went wrong.");
+      } else {
+        const errorText = await response.text();
+        console.error("Server Error:", errorText);
+        throw new Error("Internal Server Error");
+      }
     }
+
+    // DELETE biasanya status 204, tidak ada body
+    if (response.status === 204) return true;
+
+    // response JSON
+    return await response.json();
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
   }
-
-  // DELETE biasanya status 204, tidak ada body
-  if (response.status === 204) return true;
-
-  // response JSON
-  return await response.json();
 };
