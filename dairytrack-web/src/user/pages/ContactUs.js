299933fe67c7b0b5f4a26d0d9ba27ba8d8d5545b
nitasimangunsong <nitasimangunsong1@gmail.com>
2025-04-03 00:11:20 +0700

    Perbaikan tampilan contact us

--- dairytrack-web/src/user/pages/ContactUs.js
+++ dairytrack-web/src/user/pages/ContactUs.js
@@ -4,31 +4,34 @@ import './ContactUs.css';
 const ContactUs = () => {
   return (
     <div className="contact-container">
-      {/* Peta Lokasi */}
-      <div className="contact-map">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.955655519632!2d98.6253594!3d2.2886875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e0787e9196b01%3A0x67392c0157f55171!2s7JQG%2BF4H%2C%20Aek%20Nauli%20I%2C%20Kec.%20Pollung%2C%20Kabupaten%20Humbang%20Hasundutan%2C%20Sumatera%20Utara%2022456!5e0!3m2!1sen!2sid!4v1684305845722!5m2!1sen!2sid"
-          width="100%"
-          height="450"
-          style={{ border: 0 }}
-          allowFullScreen=""
-          loading="lazy"
-          title="Peta Pollung Humbang Hasundutan"
-        ></iframe>
+      {/* Container untuk peta dan form */}
+      <div className="map-form-container">
+        {/* Peta Lokasi */}
+        <div className="contact-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.955655519632!2d98.6253594!3d2.2886875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x302e0787e9196b01%3A0x67392c0157f55171!2s7JQG%2BF4H%2C%20Aek%20Nauli%20I%2C%20Kec.%20Pollung%2C%20Kabupaten%20Humbang%20Hasundutan%2C%20Sumatera%20Utara%2022456!5e0!3m2!1sen!2sid!4v1684305845722!5m2!1sen!2sid"
+            width="100%"
+            height="450"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            title="Peta Pollung Humbang Hasundutan"
+          ></iframe>
+        </div>
+
+        {/* Formulir Kontak */}
+        <div className="contact-form-container">
+          <form className="contact-form">
+            <input type="text" placeholder="Nama Anda" required />
+            <input type="email" placeholder="Email Anda" required />
+            <textarea placeholder="Pesan Anda" required></textarea>
+            <button type="submit">Kirim Pesan</button>
+          </form>
+        </div>
       </div>
 
       {/* Header "Hubungi Kami" dipindahkan ke sini */}
       <h2 className="contact-header">Hubungi Kami</h2>
-
-      {/* Formulir Kontak */}
-      <div className="contact-form-container">
-        <form className="contact-form">
-          <input type="text" placeholder="Nama Anda" required />
-          <input type="email" placeholder="Email Anda" required />
-          <textarea placeholder="Pesan Anda" required></textarea>
-          <button type="submit">Kirim Pesan</button>
-        </form>
-      </div>
 
       {/* Informasi Kontak */}
       <div className="contact-info-container">
