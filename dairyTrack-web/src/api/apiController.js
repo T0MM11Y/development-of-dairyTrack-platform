70bae2b944d61b8fe2cf90e57f505c15455bf3f2
Christian Yehezkil Gultom <111845869+T0MM11Y@users.noreply.github.com>
2025-05-12 07:33:19 -0700

    Delete dairyTrack-web directory

--- dairyTrack-web/src/api/apiController.js
+++
@@ -1,7 +0,0 @@
-const API_URL1 = 'http://localhost:5000';
-const API_URL2 = 'http://localhost:5001';
-const API_URL3 = 'http://localhost:5002';
-const API_URL4 = 'http://localhost:5003';
-
-
-export { API_URL1, API_URL2, API_URL3, API_URL4 };
