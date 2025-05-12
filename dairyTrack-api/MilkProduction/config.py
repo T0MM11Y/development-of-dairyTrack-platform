f11383c9302db46ec59ea54a1d341d34cadc3eca
T0MM11Y <tiantomm@gmail.com>
2025-05-12 22:47:00 +0700

    deploy

--- dairyTrack-api/MilkProduction/config.py
+++ dairyTrack-api/MilkProduction/config.py
@@ -4,8 +4,11 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
     DEBUG = os.environ.get('FLASK_DEBUG') or False
     TESTING = os.environ.get('FLASK_TESTING') or False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://root:@localhost/dairy_track'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disable event system to save resources
-    JSON_SORT_KEYS = False  # Prevents sorting of JSON keys in responses
+    # local development
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://root:@localhost/dairy_track'
+    
+    # production
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'mysql+pymysql://dairytrack_stiffplate:dcdd07f7b478dd1f479b4c44c4b131048a7c0ac3@xcq0v.h.filess.io:3307/dairytrack_stiffplate'
 
-# Additional configuration classes can be added here for different environments (e.g., DevelopmentConfig, ProductionConfig)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JSON_SORT_KEYS = False
