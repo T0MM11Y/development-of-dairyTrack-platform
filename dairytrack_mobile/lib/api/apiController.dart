bf1af09a18105c23e48ffdc78dca8e02e08a0e1e
Christian Yehezkil Gultom <tiantomm@gmail.com>
2025-06-25 00:11:42 +0700

    Update cow deletion logic to remove related milk production and milking session records, and adjust API URLs for consistency.

--- dairytrack_mobile/lib/api/apiController.dart
+++ dairytrack_mobile/lib/api/apiController.dart
@@ -1,6 +1,6 @@
 export 'apiController.dart';
 
-const String API_URL1 = 'http://192.168.209.161:5000';
-const String API_URL2 = 'http://192.168.209.161:5001';
-const String API_URL3 = 'http://192.168.209.161:5002/api';
-const String API_URL4 = 'http://192.168.209.161:5003';
+const String API_URL1 = 'http://192.168.29.74:5000';
+const String API_URL2 = 'http://192.168.29.74:5001';
+const String API_URL3 = 'http://192.168.29.74:5002/api';
+const String API_URL4 = 'http://192.168.29.74:5003';
