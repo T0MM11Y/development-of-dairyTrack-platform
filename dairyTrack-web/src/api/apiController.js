9792c83d3ecaff199051f8b0c6e359555d524971
Christian Yehezkil Gultom <tiantomm@gmail.com>
2025-06-29 15:15:55 +0700

    Update API URLs in web and mobile applications; add new files to .gitignore

--- dairyTrack-web/src/api/apiController.js
+++ dairyTrack-web/src/api/apiController.js
@@ -1,7 +1,6 @@
-const API_URL1 = "https://web-production-9d46e.up.railway.app";
-const API_URL2 = "https://web-production-09fef.up.railway.app";
-// const API_URL2 = "http://localhost:8000";
-const API_URL3 = "https://web-production-60d6.up.railway.app/api";
-const API_URL4 = "https://backendta-production-17e4.up.railway.app";
+const API_URL1 = "http://localhost:5000";
+const API_URL2 = "http://localhost:5001";
+const API_URL3 = "http://localhost:5002/api";
+const API_URL4 = "http://localhost:5003";
 
 export { API_URL1, API_URL2, API_URL3, API_URL4 };
--- dairytrack_mobile/lib/api/apiController.dart
+++ dairytrack_mobile/lib/api/apiController.dart
@@ -1,7 +1,6 @@
 export 'apiController.dart';
 
-
-const String API_URL1 = 'https://web-production-9d46e.up.railway.app';
-const String API_URL2 = 'https://web-production-09fef.up.railway.app';
-const String API_URL3 = 'https://web-production-60d6.up.railway.app/api';
-const String API_URL4 = 'https://backendta-production-17e4.up.railway.app';
+const String API_URL1 = 'http://192.168.156.30:5000';
+const String API_URL2 = 'http://192.168.156.30:5001';
+const String API_URL3 = 'http://192.168.156.30:5002/api';
+const String API_URL4 = 'http://192.168.156.30:5003';
